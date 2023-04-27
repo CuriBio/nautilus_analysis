2f648134f99c3892f7c639d26f26c52d907a219e
Jason Person <jrperson@gmail.com>
2023-04-27 13:58:29 -0700

    logger formatting

--- src/well_data.py
+++ src/well_data.py
@@ -12,7 +12,7 @@ from time import time
 from typing import Dict
 
 import logging
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(format='[%(asctime)s.%(msecs)03d] [well_data] [%(levelname)s] %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 log = logging.getLogger()
 
 
